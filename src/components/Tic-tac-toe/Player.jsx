3ac4eca5914d582b1a538552b9f72cb3c4432cea
AaftabAppzorro <aaftab.appzorro@gmail.com>
2024-05-15 21:53:33 +0530

    18-finished-tic-tac-toe

--- src/components/Tic-tac-toe/Player.jsx
+++ src/components/Tic-tac-toe/Player.jsx
@@ -1,11 +1,20 @@
 import { useState } from 'react';
 
-export default function Player({ initialName, symbol, isActive }) {
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
     setIsEditing((editing) => !editing);
+
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
   }
 
   function handleChange(event) {
@@ -32,5 +41,3 @@ export default function Player({ initialName, symbol, isActive }) {
     </li>
   );
 }
-
-
