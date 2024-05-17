1c779a1858093a1399f5ec5fadcf5349ad00b844
AaftabAppzorro <aaftab.appzorro@gmail.com>
2024-05-17 20:11:01 +0530

    03 lifting state up

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
+import { useState } from 'react';
 import Header from './components/Investment-Calculator/components/Header.jsx';
 import UserInput from './components/Investment-Calculator/components/UserInput.jsx';
+import Results from './components/Investment-Calculator/components/Results.jsx';
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      <Results input={userInput} />
     </>
   );
 }
