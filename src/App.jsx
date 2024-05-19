d11710d53684e3d9b3ae4d9d168502e06f6f91bf
AaftabAppzorro <aaftab.appzorro@gmail.com>
2024-05-19 18:52:46 +0530

    Section 7: Debugging 01 Starting-Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import AuthInputs from '../src/components/Styling/AuthInputs.jsx';
-import Header from '../src/components/Styling/Header.jsx';
+import { useState } from 'react';
 
-export default function App() {
+import Header from './components/Debugging/components/Header.jsx';
+import UserInput from './components/Debugging/components/UserInput.jsx';
+import Results from './components/Debugging/components/Results.jsx';
+
+function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+
   return (
     <>
       <Header />
-      <main>
-        <AuthInputs />
-      </main>
+      <UserInput userInput={userInput} onChange={handleChange} />
+      <Results input={userInput} />
     </>
   );
 }
+
+export default App;
