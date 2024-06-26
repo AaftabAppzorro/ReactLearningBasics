aec3480a6d9c9b565c3c6e9b87605224e102092e
AaftabAppzorro <aaftab.appzorro@gmail.com>
2024-06-26 22:58:58 +0530

    10-portals-finished

--- src/components/RefsPortals/ResultModal.jsx
+++ src/components/RefsPortals/ResultModal.jsx
@@ -12,7 +12,7 @@ const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref)
   });
 
   return (
-    <dialog ref={dialog} className="result-modal">
+    <dialog ref={dialog} className="result-modal"  onClose={onReset}>
       <h2>You {result}</h2>
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
