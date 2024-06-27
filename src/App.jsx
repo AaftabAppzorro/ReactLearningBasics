f3560cdf3bcc65cf831b5223b6f701e18425107c
AaftabAppzorro <aaftab.appzorro@gmail.com>
2024-06-27 18:02:04 +0530

    03-styling-buttons-inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import ProjectsSidebar from './components/PracticeProject_09/ProjectsSidebar.jsx';
+import NewProject from './components/PracticeProject_09/NewProject.jsx';
 
 function App() {
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar />
+      <NewProject />
     </main>
   );
 }
