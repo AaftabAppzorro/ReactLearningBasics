1d6b5cc7ee37b0a2b5508ec94d00fd4b999c1dc5
AaftabAppzorro <aaftab.appzorro@gmail.com>
2024-06-27 18:11:38 +0530

    04-managing-state-to-switch-cmp

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
+import { useState } from 'react';
 import ProjectsSidebar from './components/PracticeProject_09/ProjectsSidebar.jsx';
 import NewProject from './components/PracticeProject_09/NewProject.jsx';
+import NoProjectSelected from './components/PracticeProject_09/NoProjectSelected.jsx';
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NewProject />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
